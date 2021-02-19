c0ed56f97c104d9f163c6b7b6aa507b5a64edd7e
Shiinaaaaa <529772303@qq.com>
2021-02-20 00:10:04 +0800

    return

--- main.cpp
+++ main.cpp
@@ -1,16 +1,6 @@
 #include <iostream>
 #include "program.h"
 
-//改为二分查找
-//userid 的 public
-//是否判断已经登陆
-//su 能否7
-//logout
-//delete
-//if (o < tmp.nodes[0] && tmp.previous != -1) {
-//show 只查了后一个
-//mergr 问题
-
 using namespace std;
 int main() {
     string command;
@@ -30,5 +20,5 @@ int main() {
             std::cout << "Invalid\n";
         }
     }
-
+    return 0;
 }
